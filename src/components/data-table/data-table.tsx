efb9d61c039fc1d098ff6cc2b6eea3a4247a2cc7
Samudra Deka <samudra.deka@clinifyhealth.com>
2022-05-27 12:22:37 +0530

    final

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, State } from '@stencil/core';
+import { Component, Host, h, State, Prop } from '@stencil/core';
 
 @Component({
   tag: 'data-table',
@@ -8,14 +8,29 @@ export class DataTable {
   @Prop() doc: object[];
   @State() header: any;
   @State() body: object[];
+  @State() currentBody: object[];
   @State() value: string;
   @State() toggleSort = false;
   @State() currentPage = 1;
   @State() dataPerPage = 10;
 
   componentWillLoad() {
-    this.body = this.doc;
-    this.header = Object.keys(this.doc.slice(0, 1).shift()).map(title => {
+    let isObject = this.doc.every(item => typeof item === 'object');
+
+    if (isObject) {
+      this.body = this.doc;
+      this.currentBody = this.body;
+    } else {
+      this.body = this.doc.map((item, index) => {
+        return {
+          index: index + 1,
+          item,
+        };
+      });
+      this.currentBody = this.body;
+    }
+
+    this.header = Object.keys(this.body[0]).map(title => {
       return {
         title,
         sortIcon: <span class="pl-1 text-gray-500">&#8645;</span>,
@@ -26,7 +41,7 @@ export class DataTable {
 
   handleChange(event) {
     this.value = event.target.value;
-    const searchedValues = this.doc.filter(items => Object.values(items).some((item: any) => item.toString().toLowerCase().indexOf(this.value.toLowerCase()) > -1));
+    const searchedValues = this.currentBody.filter(items => Object.values(items).some((item: any) => item.toString().toLowerCase().indexOf(this.value.toLowerCase()) > -1));
     this.body = searchedValues;
     this.currentPage = 1;
   }
@@ -46,13 +61,13 @@ export class DataTable {
     let objIndex = this.header.findIndex(obj => obj.title == title);
 
     if (direction === 'none' || direction === 'desc') {
-      sortedData = this.body.sort((a, b) => a[title].localeCompare(b[title]));
+      sortedData = this.body.sort((a, b) => (a[title] > b[title] ? -1 : a[title] === b[title] ? 0 : 1));
       sortDir = 'asc';
       icon = <span class="pl-1 text-gray-500">&#8595;</span>;
     }
 
     if (direction === 'asc') {
-      sortedData = this.body.sort((b, a) => a[title].localeCompare(b[title]));
+      sortedData = this.body.sort((b, a) => (a[title] > b[title] ? -1 : a[title] === b[title] ? 0 : 1));
       sortDir = 'desc';
       icon = <span class="pl-1 text-gray-500">&#8593;</span>;
     }
@@ -77,7 +92,7 @@ export class DataTable {
             </label>
             <div class="relative mt-1">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path
                     fill-rule="evenodd"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
@@ -88,7 +103,7 @@ export class DataTable {
               <input
                 type="text"
                 id="table-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  "
                 placeholder="Search for items"
                 value={this.value}
                 onInput={e => this.handleChange(e)}
@@ -96,8 +111,8 @@ export class DataTable {
             </div>
           </div>
 
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <table class="w-full text-sm text-left text-gray-500 ">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
                 {this.header.map(item => (
                   <th scope="col" onClick={() => this.sortData(item.title, item.sortDirection)} class="px-6 py-3 cursor-pointer" title="click to sort data">
