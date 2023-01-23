f032cb63f8fd259a3a89520f3581967bbd4cfb98
Durga Phukan <durga@techvariable.com>
2023-01-23 13:59:20 +0530

    updated edit user component

--- src/components/edit-user/edit-user.tsx
+++ src/components/edit-user/edit-user.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 
 import { ClickOutside } from 'stencil-click-outside';
-// import { MultiSelectt } from '../multi-selectt/multi-selectt';
 
 @Component({
   tag: 'edit-user',
@@ -79,7 +78,7 @@ export class EditUser {
           Swal.fire({
             position: 'center',
             icon: 'success',
-            text: 'Invitation sent successfully!',
+            text: 'Roles updated successfully!',
             showConfirmButton: false,
             timer: 1500,
           });
@@ -132,19 +131,19 @@ export class EditUser {
                 </span>
 
                 <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="bg-white px-4 pt-2 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
-                      <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <div class="mx-auto pt-3 flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-sky-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                       </div>
-                      <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <div class="mt-3 text-center sm:mt-0 sm:ml-4 p-4 sm:text-left flex-grow">
                         <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
                           Edit Permissions
                         </h3>
                         <div class="mt-2">
-                          <p class="text-sm text-gray-500 mb-4">Enter email of the user.</p>
+                          <p class="text-md text-gray-500 mb-4">Enter email of the user.</p>
                           <input
                             type="email"
                             name="email"
@@ -157,12 +156,11 @@ export class EditUser {
                         </div>
 
                         <div class="w-48 ">
-                          <p class="z-10 text-sm text-gray-500 mb-4">Select permissions</p>
+                          <p class="z-10 text-md text-gray-500 mb-4">Select permissions</p>
                           <label class="block text-left">
-                            <span class="text-gray-700">Multiselect</span>
-                            <select name='role' class="form-multiselect block w-full mt-1" multiple onChange={e=>{console.log(e)}}>
+                            <select name='role' class="form-multiselect block w-full mt-1 border rounded-md" multiple onChange={e=>{console.log(e)}}>
                              {this.roles.map((role:any)=>(
-                              <option value={role.id}>{role.value}</option>
+                              <option class="px-6 py-1 hover:bg-gray-200 cursor-pointer" value={role.id}>{role.value}</option>
                              ))}
                             </select>
                           </label>
@@ -171,8 +169,8 @@ export class EditUser {
                     </div>
                   </div>
                   <div>
-                    <div class="w-full md:w-1/2 flex flex-col items-center h-64 mx-auto"></div>
-                    <div class="bg-gray-50 px-4 py-6 sm:px-6 sm:flex sm:flex-row-reverse">
+                    {/* <div class="w-full md:w-1/2 flex flex-col items-center h-64 mx-auto"></div> */}
+                    <div class="bg-gray-50 pb-4 sm:px-6 sm:flex sm:flex-row-reverse">
                       <button
                         type="submit"
                         class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:ml-3 sm:w-auto sm:text-sm"
