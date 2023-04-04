8c4df50df536476f8b12f06b53f7f545bb6ef01c
Durga <durga@techvariable.com>
2023-04-04 14:59:31 +0530

    adding validation on progress

--- src/components/editorPage/insert-node-modal/insert-node-modal.tsx
+++ src/components/editorPage/insert-node-modal/insert-node-modal.tsx
@@ -12,7 +12,7 @@ export class InsertNodeModal {
 
   @State() downloadError = null;
   @State() addNodeState = [{ property: '', value: '', valueType: '' }];
-  @State() customDropDownValue: '';
+  @State() customDropDownValue: string = '';
   @State() nodes: [{ label: string; selected: boolean }] = [{ label: '', selected: false }];
   @State() valueOptions = [
     { label: 'String', selected: true },
@@ -32,6 +32,11 @@ export class InsertNodeModal {
 
   toggleModalState() {
     this.isModalOpen = !this.isModalOpen;
+  }
+
+  discardAddNode() {
+    this.toggleModalState();
+    this.addNodeState = [{ property: '', value: '', valueType: '' }];
   }
 
   async submitHandler(e) {
@@ -62,9 +67,11 @@ export class InsertNodeModal {
   }
   optionHandler(index, selectedLabel) {
     this.addNodeState[index]['valueType'] = selectedLabel;
+    console.log('select', selectedLabel, index, this.addNodeState);
   }
 
-  handleSubmit() {
+  handleSubmit(e) {
+    console.log('Pressed', this.customDropDownValue, this.addNodeState);
     if (this.customDropDownValue !== '') {
       const properties: { [key: string]: string | number } = {};
       this.addNodeState.forEach(node => {
@@ -84,8 +91,11 @@ export class InsertNodeModal {
       state.queryMode = 'insert';
       state.insertNodeLabel = this.customDropDownValue;
       state.insertProperties = properties;
-
+      state.selectedNodeName = state.insertNodeLabel;
+      this.customDropDownValue = '';
       state.refreshData();
+      this.toggleModalState();
+      this.addNodeState = [{ property: '', value: '', valueType: '' }];
     }
   }
 
@@ -204,7 +214,7 @@ export class InsertNodeModal {
                   <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button
                       type="submit"
-                      onClick={() => this.handleSubmit()}
+                      onClick={e => this.handleSubmit(e)}
                       class="w-full gap-2 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-sky-600 text-base font-medium text-white disabled:bg-gray-200 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                     >
                       {
@@ -216,7 +226,7 @@ export class InsertNodeModal {
                     </button>
                     <button
                       type="button"
-                      onClick={() => this.toggleModalState()}
+                      onClick={() => this.discardAddNode()}
                       class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     >
                       Cancel
